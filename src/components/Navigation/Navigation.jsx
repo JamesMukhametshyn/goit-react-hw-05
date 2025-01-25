005d3c4c305e2feb91181a021e2475bfa6f810e3
JamesMukhametshyn <143405409+JamesMukhametshyn@users.noreply.github.com>
2025-01-25 13:23:32 +0100

    change

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,24 +1,22 @@
-import { NavLink } from "react-router-dom";
-import css from "./Navigation.module.css";
+import { NavLink } from "react-router-dom"
 import clsx from "clsx";
+import s from './Navigation.module.css'
+
+const buildLinkClass = ({ isActive }) => {
+    return clsx(s.link, isActive && s.active);
+}
 
 const Navigation = () => {
-  const getNavLinkClassName = ({ isActive }) =>
-    clsx(css.navLink, {
-      [css.active]: isActive,
-    });
 
-  return (
-    <nav className={css.nav}>
-      <NavLink className={getNavLinkClassName} to="/">
-        Home
-      </NavLink>
+    return (
+        <header className={s.header}>
+            <nav className={s.nav}>
+                <NavLink className={buildLinkClass} to='/'>Home</NavLink>
+                <NavLink className={buildLinkClass} to='/movies'>Movies</NavLink>
+            </nav>
+        </header>
+    )
 
-      <NavLink className={getNavLinkClassName} to="/movies">
-        Movies
-      </NavLink>
-    </nav>
-  );
-};
+}
 
-export default Navigation;
+export default Navigation
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,28 +1,12 @@
-import { useEffect, useState } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom"
 
 const NotFoundPage = () => {
-  const [timer, setTimer] = useState(0);
+    return (
+        <>
+            <h1>Not Found</h1>
+            <Link to='/'>Home</Link>
+        </>
+    )
+}
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTimer((prevTimer) => prevTimer + 1);
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  if (timer === 5) {
-    return <Navigate to="/" replace />;
-  }
-
-  return (
-    <div>
-      <h1>Sorry, this page doesn&apos;t exist (:404) </h1>
-      <h2>You will be redirected to Home page in {5 - timer} seconds.</h2>
-      <Link to="/">Go home</Link>
-    </div>
-  );
-};
-
-export default NotFoundPage;
+export default NotFoundPage
